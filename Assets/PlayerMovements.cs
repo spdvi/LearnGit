1a98e96a3e0e5fb040f49a6bbafa535da3ba349c
NeoTyger <neojl333@gmail.com>
2023-04-19 13:09:06 +0200

    Adelante y atras.

--- Assets/PlayerMovements.cs
+++ Assets/PlayerMovements.cs
@@ -15,8 +15,17 @@ public class PlayerMovements : MonoBehaviour
     {
       if (Input.GetKey(KeyCode.A)) 
       {
-        Debug.Log("Se meuve a la izqda");  
+        Debug.Log("Se mueve a la izqda");  
       }
       
+      if (Input.GetKey(KeyCode.W)) 
+      {
+        Debug.Log("Se mueve hacia adelante");  
+      }
+      
+      else if (Input.GetKey(KeyCode.S)) 
+      {
+        Debug.Log("Se mueve hacia atras");  
+      }
     }
 }
